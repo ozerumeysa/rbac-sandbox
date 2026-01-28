93820dc7c2bc873e30790943aa7b53382c76a6e7
ozerumeysa <rumeysaozer59@gmail.com>
2026-01-28 13:47:29 +0000

    project

--- repo-root/envs/projects/edc/main.tf
+++ repo-root/envs/projects/edc/main.tf
@@ -1,13 +1,11 @@
-# envs/projects/eur/main.tf
-
 # 1) Look up ISD MG by Group ID (name)
 data "azurerm_management_group" "isd" {
   name = "mg-landingzone-ISD"
 }
 
-# 2) Deploy a project (change only project_code)
-module "project" {
+# 2) Deploy this project (change only project_code)
+module "project_edc" {
   source       = "../../../modules/project"
-  project_code = "edc"  # <-- change this for each new project
+  project_code = "edc"  # <-- change this per new project
   parent_mg_id = data.azurerm_management_group.isd.id
 }
--- repo-root/modules/project/main.tf
+++ repo-root/modules/project/main.tf
@@ -5,11 +5,11 @@ terraform {
   }
 }
 
-# -------------------------
+
 # Inputs
-# -------------------------
+
 variable "project_code" {
-  description = "Short code for project (e.g., edc, dcc)"
+  description = "Short code for project (e.g., EUROM)"
   type        = string
 }
 
@@ -18,18 +18,18 @@ variable "parent_mg_id" {
   type        = string
 }
 
-# -------------------------
+
 # 1. Create project MG
-# -------------------------
+
 resource "azurerm_management_group" "project" {
   name                       = "mg-project-${var.project_code}"
   display_name               = "mg-project-${var.project_code}"
   parent_management_group_id = var.parent_mg_id
 }
 
-# -------------------------
-# 2. Create security groups
-# -------------------------
+
+# 2. Create Entra security groups
+
 locals {
   personas = {
     app           = "${var.project_code}-App"
@@ -43,14 +43,14 @@ locals {
 resource "azuread_group" "persona" {
   for_each                = local.personas
   display_name            = each.value
-  security_enabled        = true
   description             = "Persona group ${each.key} for project ${var.project_code}"
+  security_enabled        = true
   prevent_duplicate_names = true
 }
 
-# -------------------------
+
 # 3. Define RBAC roles per persona
-# -------------------------
+
 locals {
   persona_roles = {
     app = [
@@ -59,24 +59,20 @@ locals {
       "Virtual Machine User Login",
       "DevCenter Dev Box User"
     ]
-
     appreader = [
       "Reader"
     ]
-
     dataops = [
       "Storage Blob Data Owner",
       "Key Vault Contributor",
       "Storage File Data Privileged Reader"
     ]
-
     dataopsreader = [
       "Storage Blob Data Reader",
       "Storage File Data SMB Share Reader",
       "Key Vault Reader",
       "Reader"
     ]
-
     devopsauto = [
       "AcrPull",
       "AcrPush",
@@ -87,7 +83,7 @@ locals {
     ]
   }
 
-  # Expand into a flat list for for_each
+  # Flatten to [{ persona, role }, ...]
   rbac_flat = flatten([
     for persona, roles in local.persona_roles : [
       for role in roles : {
@@ -98,23 +94,20 @@ locals {
   ])
 }
 
-# -------------------------
+
 # 4. Assign RBAC at project MG
-# -------------------------
+
 resource "azurerm_role_assignment" "persona_rbac" {
-  for_each = {
-    for idx, item in local.rbac_flat :
-    idx => item
-  }
+  for_each = { for idx, item in local.rbac_flat : idx => item }
 
   scope                = azurerm_management_group.project.id
   role_definition_name = each.value.role
   principal_id         = azuread_group.persona[each.value.persona].object_id
 }
 
-# -------------------------
+
 # Outputs
-# -------------------------
+
 output "project_mg_id" {
   value = azurerm_management_group.project.id
 }
