bc1aa5b5e4c7a6b44ce89e4ce8d0babf0063519b
ozerumeysa <rumeysaozer59@gmail.com>
2026-01-28 15:38:27 +0000

    .

--- repo-root/envs/platform/main.tf
+++ repo-root/envs/platform/main.tf
@@ -12,8 +12,7 @@ module "platform" {
 
   
   pim_approver_group_object_id      = var.pim_approver_group_object_id
-  platform_policy_initiative_id     = var.platform_policy_initiative_id
-  connectivity_policy_initiative_id = var.connectivity_policy_initiative_id
+
 
 
   # Optional: override default MG names if you want different ones
