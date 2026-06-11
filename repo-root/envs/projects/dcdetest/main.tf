09e96a64e13c8ff4e241f274d8d729a71226190d
ozerumeysa <rumeysaozer59@gmail.com>
2026-06-11 11:33:18 +0100

    Update main.tf

--- repo-root/envs/projects/dcdetest/main.tf
+++ repo-root/envs/projects/dcdetest/main.tf
@@ -1,1 +1,11 @@
+# 1) Look up ISD MG by Group ID (name)
+data "azurerm_management_group" "isd" {
+  name = "mg-landingzone-ISD"
+}
 
+# 2) Deploy this project (change only project_code)
+module "project_edc" {
+  source       = "../../../modules/project"
+  project_code = "dcde"  # <-- change this per new project
+  parent_mg_id = data.azurerm_management_group.isd.id
+}
