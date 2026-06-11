644af61f7c6a68193dfc2ad49a02f41e466832d2
ozerumeysa <rumeysaozer59@gmail.com>
2026-06-11 11:33:48 +0100

    Update providers.tf

--- repo-root/envs/projects/dcdetest/providers.tf
+++ repo-root/envs/projects/dcdetest/providers.tf
@@ -1,1 +1,22 @@
+terraform {
+  required_version = ">= 1.6.0"
 
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.114"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 3.7"
+    }
+  }
+
+  backend "local" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azuread" {}
